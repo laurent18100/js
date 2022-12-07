14677633fa858c09da850f2a24d41b695b15b64a
laurent18100 <118724000+laurent18100@users.noreply.github.com>
2022-12-07 16:18:53 +0100

    exercice serie 02 js

---
+++ exercices-serie-02/exo-serie-02.js
@@ -0,0 +1,4 @@
+let prenom= prompt('Quel est votre prenom?');
+let age = prompt('quel est votre age?')
+let ageFutur = parseInt(age) + 1 ;
+console.log (`je m'appelle ${prenom}j'ai ${ageFutur}ans l'année prochaine`);
