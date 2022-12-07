0a2138a62c5916ed1cd6f04ecbada028c74c7d97
laurent18100 <118724000+laurent18100@users.noreply.github.com>
2022-12-07 16:25:10 +0100

    exo serie 02 js

---
+++ exercices-serie-02/exo-01-serie-02.js
@@ -0,0 +1,5 @@
+let nom= prompt('Quel est votre nom?');
+alert ("bienvenue "+nom+"");
+let ageFutur = parseInt(age) + 1 ;
+alert("je m'appelle "+nom+" j ai "+age+" ans l'année prochaine " + ageFutur + " ans");
+
