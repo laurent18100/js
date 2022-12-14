fa4128e6dbedebdf0a512c46c63d17ef2e1a3a5e
laurent18100 <118724000+laurent18100@users.noreply.github.com>
2022-12-14 11:24:24 +0100

    exo serise 3

--- serie-03/exo-02.js
+++ serie-03/exo-02.js
@@ -18,12 +18,29 @@ fin si
 tantque nombreEtoiles <1 ou
 nombreEtoiles >10
 */
+// faire
+let nombreEtoiles = 0;
+do
+{
+// lire nombreEtoiles
+ nombreEtoiles=prompt("saisir le nombres d'etoiles");
+// si nombreEtoiles <1 ou nombreEtoiles >10
+if ((nombreEtoiles < 1) || (nombreEtoiles > 10))
+// alors
+{console.log("le nombre d'etoiles est incorrect!");}
+// ecrire " mess erreur"
+// else
+// {console.log("le nombre d'etoiles  correct");}
 
+// fin si
+}
+while ((nombreEtoiles < 1) || (nombreEtoiles > 10))
+// tantque nombreEtoiles <1 ou
+// nombreEtoiles >10
 
 let texteEtoiles="";
 for(let i=1 ; i<=nombreEtoiles;i++){
     texteEtoiles=texteEtoiles + "*";
 }
 //alors afficher le nombre d'etoiles
-console.log("le nombre d'etoiles  correct:");
 console.log(texteEtoiles);
