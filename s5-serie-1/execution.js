c30257eed300cf128fa3a3ecdcdbbc59c4632ab0
laurent18100 <118724000+laurent18100@users.noreply.github.com>
2022-12-19 13:49:22 +0100

    image et fonction bouton

---
+++ s5-serie-1/execution.js
@@ -0,0 +1,5 @@
+let bouton = document.querySelector('#btn_cacher tout');
+bouton.addEventListener('click');
+
+let bouton1 = document.querySelector('#btn_afficher tout');
+bouton.addEventListener('click');
--- s5-serie-1/index.js
+++ s5-serie-1/index.js
@@ -19,3 +19,51 @@ let images =document.querySelectorAll('img');
 images.forEach(image=>{
     image.addEventListener('dblclick',cacherImgNone);
 });
+
+function cacherTout()
+{
+   // alert('youlou');
+   let images = document.querySelectorAll('img');
+   images.forEach(image =>{
+    image.style.display="none";
+   })
+}
+
+let boutonCacherTout = document.querySelector('#btn_cacher_tout');
+boutonCacherTout.addEventListener('click',cacherTout);
+
+
+
+function afficherTout()
+{
+    let images = document.querySelectorAll('img');
+    images.forEach(image =>{
+     image.style.display="block";
+    })
+
+}let boutonAfficherTout = document.querySelector('#btn_afficher_tout');
+boutonAfficherTout.addEventListener('click',afficherTout);
+
+function desactiverTout()
+{
+    let images = document.querySelectorAll('img');
+    images.forEach(image =>{
+        image.removeEventListener("dblclick",cacherImgNone);
+    })
+
+}
+function activerTout()
+{
+    let images = document.querySelectorAll('img');
+    images.forEach(image =>{
+        image.addEventListener("dblclick",cacherImgNone);
+    })
+
+}
+
+let boutondesactiverTout = document.querySelector('#btn_desactiver_tout');
+boutondesactiverTout.addEventListener('click',desactiverTout);
+
+let boutonactiverTout = document.querySelector('#btn_activer_tout');
+boutonactiverTout.addEventListener('click',activerTout);
+
