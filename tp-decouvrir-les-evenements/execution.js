f5a7b14d21b5e47634bb2c7220ca9b73bf47ea60
laurent18100 <118724000+laurent18100@users.noreply.github.com>
2022-12-19 08:39:21 +0100

    decouvrir les evenements  salutation

--- tp-decouvrir-les-evenements/execution.js
+++ tp-decouvrir-les-evenements/execution.js
@@ -1,3 +1,3 @@
 let bouton = document.querySelector('#btn_salutation');
 bouton.addEventListener('click',saluer);
-console.log ("abennoment du bouton au clic")
+console.log ("abonnement du bouton au clic")
